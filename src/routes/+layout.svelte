eda1c9828b38e9f5968ca32afc3b0e7e7425d491
Alek Ortiz <aleksebastian@gmail.com>
2024-03-26 19:27:31 -0500

    Added view transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
+	import { onNavigate } from '$app/navigation';
 	import Navbar from './Navbar.svelte';
 	import Drawer from './Drawer.svelte';
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 
 	let isDrawerOpen = false;
 
@@ -23,9 +35,47 @@
 </Drawer>
 
 <style>
-	:global(svg) {
-		@media (prefers-color-scheme: dark) {
-			fill: oklch(var(--bc));
+	:global(.navbar) {
+		view-transition-name: header;
+	}
+
+	:global(.drawer-side) {
+		view-transition-name: drawer;
+	}
+
+	@keyframes fade-in {
+		from {
+			opacity: 0;
 		}
+	}
+
+	@keyframes fade-out {
+		to {
+			opacity: 0;
+		}
+	}
+
+	@keyframes slide-from-right {
+		from {
+			transform: translateX(30px);
+		}
+	}
+
+	@keyframes slide-to-left {
+		to {
+			transform: translateX(-30px);
+		}
+	}
+
+	:root::view-transition-old(root) {
+		animation:
+			90ms cubic-bezier(0.4, 0, 1, 1) both fade-out,
+			300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-to-left;
+	}
+
+	:root::view-transition-new(root) {
+		animation:
+			210ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in,
+			300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-from-right;
 	}
 </style>
